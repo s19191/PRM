ddafbf4d434a364bcae10a5a61b8e0d9ac63acd3
Jan Kwasowski <61729697+s19191@users.noreply.github.com>
2021-03-25 20:53:29 +0100

    początek

---
+++ cw03/app/src/main/java/pl/edu/pja/covid_19/MainActivity.kt
@@ -0,0 +1,11 @@
+package pl.edu.pja.covid_19
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        println("onCreate")
+    }
+}

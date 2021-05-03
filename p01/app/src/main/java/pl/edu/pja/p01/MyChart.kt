7a6b9c365fba57dc7d46ed46e6147be1b8757855
Jan Kwasowski <61729697+s19191@users.noreply.github.com>
2021-05-03 02:37:50 +0200

    dobra podziałka dla sytuacji kiedy maks <=0, oraz min >=0

--- p01/app/src/main/java/pl/edu/pja/p01/MyChart.kt
+++ p01/app/src/main/java/pl/edu/pja/p01/MyChart.kt
@@ -26,49 +26,175 @@ class MyChart(context: Context, attrs: AttributeSet) : View(context, attrs) {
         canvas ?: return
         with(canvas) {
             val daysInMonth = Calendar.getInstance().getActualMaximum(Calendar.DAY_OF_MONTH)
-            var xTmp = width.toFloat() / daysInMonth
+            var xTmp = ((width.toFloat() - 50f) / daysInMonth) + 50f
             val startBalance = calculateStartBalance()
             val maximumBalance = calculateMaximumBalance()
             println("Max$maximumBalance")
             val minimumBalance = calculateMinimumBalance()
             println("Min$minimumBalance")
+            drawLine(50f, 0f, 50f, height.toFloat(), paint)
             when {
                 maximumBalance <= 0 -> {
-                    drawLine(0f, 5f, width.toFloat(), 5f, paint)
+                    drawLine(50f, 5f, width.toFloat(), 5f, paint)
                     for (i in 1..daysInMonth) {
                         drawText(i.toString().plus("\t"), xTmp, 30f, paint)
-                        xTmp += width.toFloat() / daysInMonth
+                        xTmp += (width.toFloat() - 50f) / daysInMonth
+                    }
+                    val minimumBalanceAbs = abs(minimumBalance)
+                    var yTmp = 15f
+                    when {
+                        minimumBalanceAbs >= 100000 -> {
+                            val rounded: Int = (minimumBalanceAbs.toInt() + 9999) / 10000 * 10000
+                            val howManyParts = rounded / 10000
+                            for (i in 0..rounded step 10000) {
+                                drawText("-$i", 0f, yTmp, paint)
+                                yTmp += (height.toFloat() - 20f) / howManyParts
+                            }
+                        }
+                        minimumBalanceAbs in 10000.0..100000.0 -> {
+                            val rounded: Int = (minimumBalanceAbs.toInt() + 999) / 1000 * 1000
+                            val howManyParts = rounded / 1000
+                            for (i in 0..rounded step 1000) {
+                                drawText("-$i", 0f, yTmp, paint)
+                                yTmp += (height.toFloat() - 20f) / howManyParts
+                            }
+                        }
+                        minimumBalanceAbs in 1000.0..10000.0 -> {
+                            val rounded: Int = (minimumBalanceAbs.toInt() + 99) / 100 * 100
+                            val howManyParts = rounded / 100
+                            for (i in 0..rounded step 100) {
+                                drawText("-$i", 0f, yTmp, paint)
+                                yTmp += (height.toFloat() - 20f) / howManyParts
+                            }
+                        }
+                        minimumBalanceAbs in 100.0..1000.0 -> {
+                            val rounded: Int = (minimumBalanceAbs.toInt() + 9) / 10 * 10
+                            val howManyParts = rounded / 10
+                            for (i in 0..rounded step 10) {
+                                drawText("-$i", 0f, yTmp, paint)
+                                yTmp += (height.toFloat() - 20f) / howManyParts
+                            }
+                        }
+                        minimumBalanceAbs in 10.0..100.0 -> {
+                            val rounded: Int = (minimumBalanceAbs.toInt() + 9) / 10 * 10
+                            val howManyParts = rounded
+                            for (i in 0..rounded step 1) {
+                                drawText("-$i", 0f, yTmp, paint)
+                                yTmp += (height.toFloat() - 20f) / howManyParts
+                            }
+                        }
                     }
                 }
                 minimumBalance >= 0 -> {
-                    drawLine(0f, height.toFloat() - 5f, width.toFloat(), height.toFloat() - 5f, paint)
+                    drawLine(50f, height.toFloat() - 5f, width.toFloat(), height.toFloat() - 5f, paint)
                     for (i in 1..daysInMonth) {
                         drawText(i.toString().plus("\t"), xTmp, height.toFloat() - 30f, paint)
-                        xTmp += width.toFloat() / daysInMonth
+                        xTmp += (width.toFloat() - 50f) / daysInMonth
+                    }
+                    var yTmp = 5f
+                    when {
+                        maximumBalance >= 100000 -> {
+                            val rounded: Int = (maximumBalance.toInt() + 9999) / 10000 * 10000
+                            val howManyParts = rounded / 10000
+                            for (i in 0..rounded step 10000) {
+                                drawText(i.toString(), 0f, height.toFloat() - yTmp, paint)
+                                yTmp += (height.toFloat() - 20f) / howManyParts
+                            }
+                        }
+                        maximumBalance in 10000.0..100000.0 -> {
+                            val rounded: Int = (maximumBalance.toInt() + 999) / 1000 * 1000
+                            val howManyParts = rounded / 1000
+                            for (i in 0..rounded step 1000) {
+                                drawText(i.toString(), 0f, height.toFloat() - yTmp, paint)
+                                yTmp += (height.toFloat() - 20f) / howManyParts
+                            }
+                        }
+                        maximumBalance in 1000.0..10000.0 -> {
+                            val rounded: Int = (maximumBalance.toInt() + 99) / 100 * 100
+                            val howManyParts = rounded / 100
+                            for (i in 0..rounded step 100) {
+                                drawText(i.toString(), 0f, height.toFloat() - yTmp, paint)
+                                yTmp += (height.toFloat() - 20f) / howManyParts
+                            }
+                        }
+                        maximumBalance in 100.0..1000.0 -> {
+                            val rounded: Int = (maximumBalance.toInt() + 9) / 10 * 10
+                            val howManyParts = rounded / 10
+                            for (i in 0..rounded step 10) {
+                                drawText(i.toString(), 0f, height.toFloat() - yTmp, paint)
+                                yTmp += (height.toFloat() - 20f) / howManyParts
+                            }
+                        }
+                        maximumBalance in 10.0..100.0 -> {
+                            val rounded: Int = (maximumBalance.toInt() + 9) / 10 * 10
+                            val howManyParts = rounded
+                            for (i in 0..rounded step 1) {
+                                drawText(i.toString(), 0f, height.toFloat() - yTmp, paint)
+                                yTmp += (height.toFloat() - 20f) / howManyParts
+                            }
+                        }
                     }
                 }
                 else -> {
-                    if (maximumBalance >= abs(minimumBalance)) {
+                    val sumBalanceAbs = maximumBalance + abs(minimumBalance)
+                    var rounded: Int
+                    var howManyParts: Int
+                    when {
+                        sumBalanceAbs >= 100000 -> {
+                            rounded = (sumBalanceAbs.toInt() + 9999) / 10000 * 10000
+                            val howManyParts = rounded / 10000
+                        }
+                        sumBalanceAbs in 10000.0..100000.0 -> {
+                            rounded = (sumBalanceAbs.toInt() + 999) / 1000 * 1000
+                            howManyParts = rounded / 1000
+                        }
+                        sumBalanceAbs in 1000.0..10000.0 -> {
+                            rounded = (sumBalanceAbs.toInt() + 99) / 100 * 100
+                            howManyParts = rounded / 100
+                        }
+                        sumBalanceAbs in 100.0..1000.0 -> {
+                            rounded = (sumBalanceAbs.toInt() + 9) / 10 * 10
+                            howManyParts = rounded / 10
+                        }
+                        sumBalanceAbs in 10.0..100.0 -> {
+                            rounded = (sumBalanceAbs.toInt() + 9) / 10 * 10
+                            howManyParts = rounded
+                        }
+                    }
+                        if (maximumBalance >= abs(minimumBalance)) {
                         drawLine(
-                            0f,
-                            height.toFloat() * (abs(minimumBalance)/(maximumBalance + abs(minimumBalance))).toFloat(),
+                            50f,
+                            height.toFloat() * (abs(minimumBalance) / sumBalanceAbs).toFloat(),
                             width.toFloat(),
-                            height.toFloat() * (abs(minimumBalance)/(maximumBalance + abs(minimumBalance))).toFloat(),
-                            paint)
+                            height.toFloat() * (abs(minimumBalance) / sumBalanceAbs).toFloat(),
+                            paint
+                        )
                         for (i in 1..daysInMonth) {
-                            drawText(i.toString().plus("\t"), xTmp, height.toFloat() * (abs(minimumBalance)/(maximumBalance + abs(minimumBalance))).toFloat() + 30f, paint)
-                            xTmp += width.toFloat() / daysInMonth
+                            drawText(
+                                i.toString().plus("\t"), xTmp, height.toFloat() * (abs(
+                                    minimumBalance
+                                ) / (maximumBalance + abs(minimumBalance))).toFloat() + 30f, paint
+                            )
+                            xTmp += (width.toFloat() - 50f) / daysInMonth
                         }
                     } else {
                         drawLine(
-                            0f,
-                            height.toFloat() * (maximumBalance/(abs(minimumBalance) + maximumBalance)).toFloat(),
+                            50f,
+                            height.toFloat() * (maximumBalance / sumBalanceAbs).toFloat(),
                             width.toFloat(),
-                            height.toFloat() * (maximumBalance/(abs(minimumBalance) + maximumBalance)).toFloat(),
-                            paint)
+                            height.toFloat() * (maximumBalance / sumBalanceAbs).toFloat(),
+                            paint
+                        )
                         for (i in 1..daysInMonth) {
-                            drawText(i.toString().plus("\t"), xTmp, height.toFloat() * (maximumBalance/(abs(minimumBalance) + maximumBalance)).toFloat() + 30f, paint)
-                            xTmp += width.toFloat() / daysInMonth
+                            drawText(
+                                i.toString().plus("\t"),
+                                xTmp,
+                                height.toFloat() * (maximumBalance / (abs(
+                                    minimumBalance
+                                ) + maximumBalance)).toFloat() + 30f,
+                                paint
+                            )
+                            xTmp += (width.toFloat() - 50f) / daysInMonth
                         }
                     }
                 }

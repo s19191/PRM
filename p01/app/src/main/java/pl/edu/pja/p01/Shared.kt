acb6b7ecd5f54c87c357f282663a7a826aecc3e8
Jan Kwasowski <61729697+s19191@users.noreply.github.com>
2021-05-03 18:14:05 +0200

    dodane dane testowe

--- p01/app/src/main/java/pl/edu/pja/p01/Shared.kt
+++ p01/app/src/main/java/pl/edu/pja/p01/Shared.kt
@@ -1,7 +1,11 @@
 package pl.edu.pja.p01
 
 import pl.edu.pja.p01.model.Expense
+import java.util.*
 
 object Shared {
-    val expenseList = mutableListOf<Expense>()
+    val expenseList = mutableListOf<Expense>(
+            Expense("Praca", 1000.0, 1L,  Calendar.getInstance().also { calendar -> calendar.set(2021, 3, 3) }.timeInMillis),
+            Expense("PJATK", -1500.0, 1L,  Calendar.getInstance().also { calendar -> calendar.set(2021, 3, 4) }.timeInMillis)
+    )
 }

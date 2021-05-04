d73a3c1adb038ee6d04f883f700e1b76d7525e4b
Jan Kwasowski <61729697+s19191@users.noreply.github.com>
2021-05-05 01:32:56 +0200

    zmiana nazwy, update android studio

--- p01/app/src/main/java/pl/edu/pja/p01/shared/Shared.kt
+++ p01/app/src/main/java/pl/edu/pja/p01/shared/Shared.kt
@@ -5,8 +5,8 @@ import java.util.*
 
 object Shared {
     val expenseList = mutableListOf<Expense>(
-            Expense("PJATK", -1500.0, 1L,  Calendar.getInstance().also { calendar -> calendar.set(2021, 3, 4) }.timeInMillis),
-            Expense("Praca", 2000.0, 1L,  Calendar.getInstance().also { calendar -> calendar.set(2021, 3, 3) }.timeInMillis),
-            Expense("PJATK", -1500.0, 1L,  Calendar.getInstance().also { calendar -> calendar.set(2021, 2, 4) }.timeInMillis)
+        Expense("PJATK", -1500.0, 1L,  Calendar.getInstance().also { calendar -> calendar.set(2021, 2, 4) }.timeInMillis),
+        Expense("PJATK", 3000.0, 1L,  Calendar.getInstance().also { calendar -> calendar.set(2021, 3, 4) }.timeInMillis),
+        Expense("Praca", -2000.0, 1L,  Calendar.getInstance().also { calendar -> calendar.set(2021, 3, 10) }.timeInMillis)
     )
 }

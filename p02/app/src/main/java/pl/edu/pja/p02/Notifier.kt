48943709a1828e2f1c9475e3b9c2bbb01e5950d8
Jan Kwasowski <61729697+s19191@users.noreply.github.com>
2021-05-31 00:21:26 +0200

    straszliwy bałagan

---
+++ p02/app/src/main/java/pl/edu/pja/p02/Notifier.kt
@@ -0,0 +1,30 @@
+package pl.edu.pja.p02
+
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import com.google.android.gms.location.GeofencingEvent
+
+class Notifier  : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val event = GeofencingEvent.fromIntent(intent)
+        println("Poza domem!")
+//        val event = intent?.let {
+//            GeofencingEvent.fromIntent(it)
+//        }
+//        event?.errorCode
+//
+//        event?.triggeringGeofences?.first()?.requestId
+//        context?.let {
+//
+//            val notification = NotificationCompat.Builder(it, "pl.edu.pja.kwarantanna13.Geofence")
+//                .setSmallIcon(R.drawable.ic_launcher_foreground)
+//                .setContentTitle("Wyszedłeś z kwarantanny")
+//                .build()
+//            it.getSystemService(NotificationManager::class.java)
+//                ?.notify(1, notification)
+//        }
+    }
+}
